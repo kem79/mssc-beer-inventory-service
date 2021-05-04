aaea5f0ac0df5d39093fd257322b1085c7813d1a
Marc <marc.marechal@dell.com>
2021-05-04 10:39:47 +0800

    Implement New Inventory Listener

--- src/main/java/guru/sfg/beer/inventory/service/service/NewInventoryListener.java
+++ src/main/java/guru/sfg/beer/inventory/service/service/NewInventoryListener.java
@@ -15,7 +15,7 @@ import org.springframework.stereotype.Component;
 @Slf4j
 @RequiredArgsConstructor
 @Component
-public class NewServiceListener {
+public class NewInventoryListener {
 
     public final BeerInventoryRepository beerInventoryRepository;
 
--- src/main/java/guru/sfg/common/events/NewInventoryEvent.java
+++ src/main/java/guru/sfg/common/events/NewInventoryEvent.java
@@ -1,8 +1,11 @@
 package guru.sfg.common.events;
 
+import lombok.NoArgsConstructor;
+
 /**
  * Created by marecm on 4/27/2021
  */
+@NoArgsConstructor
 public class NewInventoryEvent extends BeerEvent {
     public NewInventoryEvent(BeerDto beerDto) {
         super(beerDto);
