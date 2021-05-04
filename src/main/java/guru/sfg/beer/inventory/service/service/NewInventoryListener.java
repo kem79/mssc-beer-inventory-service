a99357ad06f767c1e3c249d75f3afcedf2a0118a
Marc <marc.marechal@dell.com>
2021-05-04 11:31:57 +0800

    Refactor the Model to a Common Package

--- src/main/java/guru/sfg/beer/inventory/service/service/NewInventoryListener.java
+++ src/main/java/guru/sfg/beer/inventory/service/service/NewInventoryListener.java
@@ -3,7 +3,7 @@ package guru.sfg.beer.inventory.service.service;
 import guru.sfg.beer.inventory.service.config.JmsConfig;
 import guru.sfg.beer.inventory.service.domain.BeerInventory;
 import guru.sfg.beer.inventory.service.repositories.BeerInventoryRepository;
-import guru.sfg.common.events.NewInventoryEvent;
+import guru.sfg.brewery.model.event.NewInventoryEvent;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.jms.annotation.JmsListener;
